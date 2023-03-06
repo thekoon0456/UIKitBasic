1410b32e47a3a6970d76a34ea7ee0246690d95fd
thekoon0456 <thekoon0456@gmail.com>
2023-03-06 19:43:33 +0900

    [Refactor] View 분리

--- UIKitCrashCourse/UIKitCrashCourse/PeopleViewController.swift
+++ UIKitCrashCourse/UIKitCrashCourse/PeopleViewController.swift
@@ -9,14 +9,10 @@ import UIKit
 
 class PeopleViewController: UIViewController {
 
-
     //UIView만들고 다른 모든 요소에 대한 컨터이너로 사용
-    private lazy var personContainerVw: UIView = {
+    private lazy var personVw: PersonView = {
         
-        let vw = UIView()
-        vw.layer.cornerRadius = 10
-        vw.backgroundColor = UIColor.gray.withAlphaComponent(0.1)
-        vw.translatesAutoresizingMaskIntoConstraints = false
+        let vw = PersonView()
         return vw
     }()
     
@@ -38,37 +34,18 @@ private extension PeopleViewController {
         
         //실제 뷰 구현
         //1.UIVew 먼저 추가
-        self.view.addSubview(personContainerVw)
-        //2.내부에 stackView추가, stackView는 쌓이는 만큼 높이 알아서 조정하므로 크기 따로 설정할 필요 없음
-        personContainerVw.addSubview(personStackVw)
+        self.view.addSubview(personVw)
         
-        personStackVw.addArrangedSubview(nameLBl)
-        personStackVw.addArrangedSubview(emailLBl)
-        personStackVw.addArrangedSubview(subscribeBtn)
-        
-        //오토 레이아웃 구성 (제약조건 적용)
-        //personContainerVw에 제약조건 추가하고
-        //컨테이너 내의 스택뷰 추가
         NSLayoutConstraint.activate([
             //personContainerVw 추가하고 중앙정렬
-            personContainerVw.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            personVw.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             //center로 안하고 leading, trailing 고정해서 꽉 채움 /constant로 padding처럼 사용. 밀어넣으므로 trailing방향에서는 음수
-            personContainerVw.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
-            personContainerVw.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
-            
-            //스택뷰 추가하고 모든 레이아웃을 측면에 고정
-            personStackVw.topAnchor.constraint(equalTo: personContainerVw.topAnchor, constant: 8),
-            personStackVw.bottomAnchor.constraint(equalTo: personContainerVw.bottomAnchor, constant: -8),
-            personStackVw.leadingAnchor.constraint(equalTo: personContainerVw.leadingAnchor, constant: 8),
-            personStackVw.trailingAnchor.constraint(equalTo: personContainerVw.trailingAnchor, constant: -8),
-            
+            personVw.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            personVw.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
         ])
         
     }
     
-    @objc func didTapSubscribe(sender: UIButton) {
-        print("구독함")
-    }
 }
 
 
--- UIKitCrashCourse/UIKitCrashCourse/PersonView.swift
+++ UIKitCrashCourse/UIKitCrashCourse/PersonView.swift
@@ -74,6 +74,36 @@ class PersonView: UIView {
 private extension PersonView {
     
     func setup() {
+        self.layer.cornerRadius = 10
+        self.backgroundColor = UIColor.gray.withAlphaComponent(0.1)
+        self.translatesAutoresizingMaskIntoConstraints = false
         
+        //2.내부에 stackView추가, stackView는 쌓이는 만큼 높이 알아서 조정하므로 크기 따로 설정할 필요 없음
+        self.addSubview(personStackVw)
+        
+        personStackVw.addArrangedSubview(nameLBl)
+        personStackVw.addArrangedSubview(emailLBl)
+        personStackVw.addArrangedSubview(subscribeBtn)
+        
+        //오토 레이아웃 구성 (제약조건 적용)
+        //personContainerVw에 제약조건 추가하고
+        //컨테이너 내의 스택뷰 추가
+        NSLayoutConstraint.activate([
+            //            //personContainerVw 추가하고 중앙정렬
+            //            personContainerVw.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            //            //center로 안하고 leading, trailing 고정해서 꽉 채움 /constant로 padding처럼 사용. 밀어넣으므로 trailing방향에서는 음수
+            //            personContainerVw.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            //            personContainerVw.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            
+            //스택뷰 추가하고 모든 레이아웃을 측면에 고정
+            personStackVw.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+            personStackVw.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
+            personStackVw.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            personStackVw.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
+        ])
+    }
+    
+    @objc func didTapSubscribe(sender: UIButton) {
+        print("구독함")
     }
 }
