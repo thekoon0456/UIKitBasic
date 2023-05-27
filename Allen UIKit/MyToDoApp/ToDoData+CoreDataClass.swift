5031aa1744a9b6378c03e135613dd06d2da50cf6
thekoon0456 <thekoon0456@gmail.com>
2023-05-27 18:09:39 +0900

    [Add] TodoApp

---
+++ Allen UIKit/MyToDoApp/ToDoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoData+CoreDataClass.swift
+//  MyToDoApp
+//
+//  Created by Deokhun KIM on 2023/05/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoData)
+public class ToDoData: NSManagedObject {
+
+}
