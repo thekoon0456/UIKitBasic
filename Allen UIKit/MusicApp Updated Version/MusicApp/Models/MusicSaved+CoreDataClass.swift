07e87a0c7731f11f0c2d81dfe53552ed6094552d
thekoon0456 <thekoon0456@gmail.com>
2023-05-28 19:58:10 +0900

    [Add] MusicApp

---
+++ Allen UIKit/MusicApp Updated Version/MusicApp/Models/MusicSaved+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MusicSaved+CoreDataClass.swift
+//  MusicApp
+//
+//  Created by Deokhun KIM on 2023/05/27.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MusicSaved)
+public class MusicSaved: NSManagedObject {
+
+}
