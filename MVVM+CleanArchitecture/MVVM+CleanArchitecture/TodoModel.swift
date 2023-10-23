bc06d397c809d7a9e39634e1bcef6ceadf915557
thekoon0456 <thekoon0456@gmail.com>
2023-10-23 15:04:44 +0900

    [Add] Model

---
+++ MVVM+CleanArchitecture/MVVM+CleanArchitecture/TodoModel.swift
@@ -0,0 +1,14 @@
+//
+//  TodoModel.swift
+//  MVVM+CleanArchitecture
+//
+//  Created by Deokhun KIM on 10/23/23.
+//
+
+import Foundation
+
+struct Todo {
+    var id: String
+    var title: String
+    var isCompleted: Bool
+}
