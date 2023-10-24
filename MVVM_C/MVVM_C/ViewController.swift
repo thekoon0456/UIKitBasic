6cc7f10f7d16a82efc87524253eda4d1dd924b52
thekoon0456 <thekoon0456@gmail.com>
2023-10-24 17:20:31 +0900

    [Fix] 패키지 오류로 다시 프로젝트 생성

---
+++ MVVM_C/MVVM_C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_C
+//
+//  Created by Deokhun KIM on 10/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
